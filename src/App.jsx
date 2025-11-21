6ca60f6b1914f206fe82598c71efe13899dc561b
jeonginwoo <jiw413@naver.com>
2025-11-21 14:20:07 +0900

    update: 셋팅 데이터 추가하기 위해 localStorage 데이터 형태 변경

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,32 @@ import ControllerArea from "@/components/ControllerArea";
 
 export default function App() {
   const localStorage = {
-    "34ea2a834c0022212290c26ac5e170a1": { zoneId: 1, platform: "chzzk" },
-    "b3e262a2795f17734c149afc738ad250": { platform: "chzzk" },
-    "b2854dc0735e55fa86c53bd15242d30f": { platform: "chzzk" },
-    "6086f17b054010b0657af00aff6e6d05": { platform: "chzzk" },
-    "93fe884808459fb4e4a3c7d64f0eef03": { platform: "chzzk" },
-    "80b36a0ae8e887e893ce0014dbfece4a": { platform: "chzzk" },
-    "5f800579267362c952f76f3c6fe695b2": { platform: "chzzk" },
-    "60e2a319d889b3ef6979f68dc3c3fd79": { platform: "chzzk" },
-    "f2607c885c65b6776b9cf5bfb473753c": { platform: "chzzk" },
-    "w2rdoo": { platform: "soop" },
-    "ooojoijo": { platform: "soop" },
+    channels: {
+      "34ea2a834c0022212290c26ac5e170a1": { zoneId: 1, platform: "chzzk" },
+      "b3e262a2795f17734c149afc738ad250": { platform: "chzzk" },
+      "b2854dc0735e55fa86c53bd15242d30f": { platform: "chzzk" },
+      "6086f17b054010b0657af00aff6e6d05": { platform: "chzzk" },
+      "93fe884808459fb4e4a3c7d64f0eef03": { platform: "chzzk" },
+      "80b36a0ae8e887e893ce0014dbfece4a": { platform: "chzzk" },
+      "5f800579267362c952f76f3c6fe695b2": { platform: "chzzk" },
+      "60e2a319d889b3ef6979f68dc3c3fd79": { platform: "chzzk" },
+      "f2607c885c65b6776b9cf5bfb473753c": { platform: "chzzk" },
+      "w2rdoo": { platform: "soop" },
+      "ooojoijo": { platform: "soop" },
+    },
+    layout: "layout1",
   };
 
   const [channels, setChannels] = useState({});
   const [pointerEventsEnabled, setPointerEventsEnabled] = useState(false);
-  const [layoutType, setLayoutType] = useState("layout1");
+  const [layoutType, setLayoutType] = useState(localStorage.layout || "layout1");
   const canvasRef = useRef(null);
 
   /** 📦 초기 데이터 + 라이브 상태 주기적 갱신 */
   useEffect(() => {
     const fetchInitialChannels = async () => {
       try {
-        const data = await getAllChannelsData(localStorage);
+        const data = await getAllChannelsData(localStorage.channels);
         setChannels(data);
       } catch (error) {
         console.error("❌ 초기 채널 데이터 불러오기 실패:", error);
@@ -39,7 +42,7 @@ export default function App() {
 
     const updateLiveStatus = async () => {
       try {
-        const entries = Object.entries(localStorage);
+        const entries = Object.entries(localStorage.channels);
         await Promise.all(
           entries.map(async ([channelId, item]) => {
             try {
